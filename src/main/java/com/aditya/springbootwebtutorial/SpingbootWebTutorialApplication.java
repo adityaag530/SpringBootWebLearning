a2ba29b20992eacd9cdc24eae4cdc07d7b9b52dd
adityaag530 <120978306+adityaag530@users.noreply.github.com>
2025-04-12 20:55:30 +0530

    initial setup

---
+++ src/main/java/com/aditya/springbootwebtutorial/SpingbootWebTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.springbootwebtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootWebTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootWebTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aditya/springbootwebtutorial/SpingbootWebTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.springbootwebtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootWebTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
